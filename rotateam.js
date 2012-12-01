804d6ffd6f73a61866a50b6dbebddefba444c013
Geoff Shannon <earthlingzephyr@gmail.com>
2012-11-30 22:47:42 -0800

    Rename form id to more generic

--- rotateam.js
+++ rotateam.js
@@ -1,24 +1,30 @@
 Teams = new Meteor.Collection("teams");
 Members = new Meteor.Collection("members");
 
+currentGroup = '';
 
 if (Meteor.isClient) {
 
     Template.nav.groups = function () {
         return Teams.find();
     };
+
     Template.nav.events({
         'click #add-group': function (event) {
             console.log("Clicked the add-group button");
         },
         'click a': function (event) {
             console.log("Show group "+this.name);
+            if (this.name) {
+                currentGroup = this.name;
+                console.log(currentGroup);
+            }
         }
     });
 
-    Template.addGroup.events({
-        'click #add-group-form button': function (event) {
-            var groupName = $('#add-group-form')[0].elements["groupname"].value;
+    Template.addForm.events({
+        'click #add-form button': function (event) {
+            var groupName = $('#add-form')[0].elements["groupname"].value;
             console.log("Added a group "+groupName+"!");
             Teams.insert({'name': groupName,
                           'index': 0,
@@ -31,6 +37,10 @@ if (Meteor.isClient) {
                          });
         }
     });
+
+    Template.body.members = function() {
+        return Members.find({'group': currentGroup});
+    };
 }
 
 if (Meteor.isServer) {
