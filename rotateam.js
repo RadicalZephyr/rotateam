6c417d9ce4cee6cd4d770d67f486dabb4846b66b
Geoff Shannon <earthlingzephyr@gmail.com>
2012-11-30 22:37:06 -0800

    Get add group working

--- rotateam.js
+++ rotateam.js
@@ -8,11 +8,29 @@ if (Meteor.isClient) {
         return Teams.find();
     };
     Template.nav.events({
-        'click #add-group': function () {
+        'click #add-group': function (event) {
             console.log("Clicked the add-group button");
+        },
+        'click a': function (event) {
+            console.log("Show group "+this.name);
         }
     });
 
+    Template.addGroup.events({
+        'click #add-group-form button': function (event) {
+            var groupName = $('#add-group-form')[0].elements["groupname"].value;
+            console.log("Added a group "+groupName+"!");
+            Teams.insert({'name': groupName,
+                          'index': 0,
+                          'members': [],
+                          'period': ""},
+                         function (err, id) {
+                             if (err) {
+                                 console.warn("Unable to create group with name: "+groupName);
+                             }
+                         });
+        }
+    });
 }
 
 if (Meteor.isServer) {
